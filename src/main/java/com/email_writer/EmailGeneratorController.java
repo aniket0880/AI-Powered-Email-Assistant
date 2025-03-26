b5729fbab97b69efc83c991d29658134a6e59b04
aniket0880 <aabaravkar456@gmail.com>
2025-03-27 00:59:40 +0530

    initial commit

---
+++ src/main/java/com/email_writer/EmailGeneratorController.java
@@ -0,0 +1,18 @@
+package com.email_writer;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailGeneratorController {
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response
+        return ResponseEntity.ok("");
+    }
+}
